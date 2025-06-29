c5d881d540e7e8d4a3208c2cf2cebb59f406bb92
LeonAdeoye <leon.adeoye@gmail.com>
2025-06-29 21:14:32 +0800

    new data structures

--- src/main/java/com/leon/TagExtractor.java
+++ src/main/java/com/leon/TagExtractor.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class TagExtractor {
-    public static List<String> extractTag(List<String> xmlList, String tagName) {
+    public static List<String> extractTagFromList(List<String> xmlList, String tagName) {
         List<String> tagValues = new ArrayList<>();
         String regex = String.format("<%s>(.*?)</%s>", tagName, tagName);
         Pattern pattern = Pattern.compile(regex);
@@ -19,13 +19,29 @@ public class TagExtractor {
         return tagValues;
     }
 
+    public static List<String> extractTagFromString(String xml, String tagName) {
+        List<String> tagValues = new ArrayList<>();
+        String regex = String.format("<%s>(.*?)</%s>", tagName, tagName);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(xml);
+
+        while (matcher.find()) {
+            tagValues.add(matcher.group(1));
+        }
+
+        return tagValues;
+    }
+
+
     public static void main(String[] args) {
         List<String> xmls = List.of(
                 "<StrategyName>Alpha</StrategyName>",
                 "<StrategyName>Beta</StrategyName>",
                 "<StrategyName>Gamma</StrategyName>"
         );
+        System.out.println(extractTagFromList(xmls, "StrategyName"));
 
-        System.out.println(extractTag(xmls, "StrategyName"));
+        String xml =  "<StrategyName>Alpha</StrategyName><StrategyName>Beta</StrategyName><StrategyName>Gamma</StrategyName>";
+        System.out.println(extractTagFromString(xml, "StrategyName"));
     }
 }
